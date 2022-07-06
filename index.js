6c7d86830d9f0c1fb50c7b0b8a34cdb7e89af964
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-07-06 11:15:44 +0100

    pw

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
-const router = require('./router')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const router = require('./router');
+const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(router)
 
+mongoose.connect("mongodb+srv://alyzande:password@cluster0.khie2.mongodb.net/?retryWrites=true&w=majority")
 app.listen(port, () => 
 console.log(`Example app listening at http://localhost:${port}`))
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log("database connected")
+});
 
