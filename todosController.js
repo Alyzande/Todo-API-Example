89447c73e0477b0caa88b747e4863eaeb287ad5a
Alyzande <44140612+Alyzande@users.noreply.github.com>
2022-07-06 14:53:43 +0100

    modified exports.show and .create

--- todosController.js
+++ todosController.js
@@ -1,49 +1,51 @@
 const createError = require('http-errors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://alyzande2:password2@cluster0.khie2.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const uri = "mongodb+srv://alyzande2:password2@cluster0.khie2.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+const {Todo} = require ('./models/todos')
 
-// let todolist = []
-// let idno = 0
 
 exports.index = function (req, res) {
   //old connection
-  // res.send(todolist)
-  client.connect (async (err) => {
-    const findResult = client.db('myFirstDatabase').collection('todos').find();
-    const result = await findResult.toArray();
-    res.send(result);
-  });
+  //  client.connect (async (err) => {
+  //   const findResult = client.db('mySecondDatabase').collection('todos').find();
+  //   const result = await findResult.toArray();
+  //   res.send(result);
+  // });
+  Todo.find()
+  .then(todos =>
+    res.send(todos))
 }
 
 exports.create = function (req, res, next) {
   if (!req.body.name) {
     return (next(createError(400, "name is required")))
   }
-  client.connect ((err) => {
-    client.db('myFirstDatabase')
-    .collection('todos')
-    .insertOne({
-      // id: idno,
-      name: req.body.name,
-      description: req.body.description 
-    })
-    .then  ( () => {res.send({result:true})})
-  });
+  // client.connect ((err) => {
+  //   client.db('myFirstDatabase')
+  //   .collection('todos')
+  //   .insertOne({
 
-  // old connection
-  // todolist.push({ id: idno, name: req.body.name })
-  // res.send({ result: true })
-  // idno++
+  //     name: req.body.name,
+  //     description: req.body.description 
+  //   })
+  //   .then  ( () => {res.send({result:true})})
+  // });
+  const todo = new Todo ({ name: req.body.name})
+  todo.save()
+  .then(() => res.send({ result: true }))
 }
 
-// exports.show = function (req, res, next) {
-//   const todoitem = todolist.find((todo) => todo.id == req.params.id)
-//   if (!todoitem) {
-//     return (next(createError(404, "no todo with that id")))
-//   }
-//   res.send(todoitem)
-// }
+exports.show = function(req, res, next) {
+  Todo.findOne({ _id: ObjectId(req.params.id)})
+  .then((todoitem) => {
+    console.log(todoitem)
+    if (!todoitem) {
+      return (next(createErrror(404, "no todo with that id")))
+    }
+    res.send(todoitem)
+  })
+}
 
 exports.delete = function (req, res, next) {
   client.connect (async (err) => {
